4d54f28a11658ee3378e041c3044f467462cc4a1
snimmag6 <sai.nimmagadda@optum.com>
2019-03-17 13:05:47 -0400

    Add elastic ip w/ implicit dependency in terraform definition

--- example_aws_instance.tf
+++ example_aws_instance.tf
@@ -10,3 +10,7 @@ resource "aws_instance" "terraform_example" {
     ami           = "ami-b374d5a5"
     instance_type = "t2.micro"
 }
+
+resource "aws_eip" "ip" {
+    instance = "${aws_instance.terraform_example.id}"
+}
