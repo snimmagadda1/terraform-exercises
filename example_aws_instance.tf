a1d5072bb5fc790a1be0c160b025a6e92176f891
snimmag6 <sai.nimmagadda@optum.com>
2019-03-17 13:18:48 -0400

    Example of provisioner w/ ec2 instance

--- example_aws_instance.tf
+++ example_aws_instance.tf
@@ -9,6 +9,10 @@
 resource "aws_instance" "terraform_example" {
     ami           = "ami-b374d5a5"
     instance_type = "t2.micro"
+
+    provisioner "local-exec" {
+    command = "echo ${aws_instance.terraform_example.public_ip} > ip_address.txt"
+}
 }
 
 resource "aws_eip" "ip" {
