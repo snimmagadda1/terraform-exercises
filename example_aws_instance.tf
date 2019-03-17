c1abc9c914814c641f990def0b04afecda7c00f1
snimmag6 <sai.nimmagadda@optum.com>
2019-03-17 12:25:21 -0400

    First example - an ec2 basic instances

---
+++ example_aws_instance.tf
@@ -0,0 +1,12 @@
+
+# Provide credentials here (by default Terraform will search for saved Credentials in ~/.aws/credentials) if not provided below
+# provider "aws" {
+#     access_key = "ACCESS_KEY_HERE"
+#     secret_key = "SECRET_KEY_HERE"
+#     region     = "us-east-1"
+# }
+
+resource "aws_instance" "terraform_example" {
+    ami           = "ami-2757f631"
+    instance_type = "t2.micro"
+}
